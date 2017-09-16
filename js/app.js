e69a23d21779f0870b33f391a7151e1be41a0dc0
Jenny-Ha <jennyha069@gmail.com>
2017-09-16 11:22:25 -0500

    Add Avances

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@ $(document).ready(function(){
     var bg_area = $(".bg-area");
     btn.click(function() {
         bg_color = $(this).css("background-color");
-        //bg_area.style.backgroundColor = bg_color;
         bg_area.css("backgroundColor", bg_color);
     });    
  });
